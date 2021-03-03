bfaba5e7dabbd5ed93e5695067441fb21b01ee3b
Guilherme Barbosa <64219293+RubberMass@users.noreply.github.com>
2021-03-03 13:10:35 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -1,21 +1,26 @@
 const express = require('express')
 const routes = express.Router()
-
 const teachers = require('./teachers')
 
 routes.get('/', function(req, res) {
-    return res.redirect("/instructors")
+    return res.redirect("/teachers")
 })
 
-routes.get('/instructors', function(req, res) {
-    return res.render("instructors/index")
+routes.get('/teachers', function(req, res) {
+    return res.render("teachers/index")
 })
 
-routes.get('/instructors/create', function(req, res) {
-    return res.render("instructors/create")
+routes.get('/teachers/create', function(req, res) {
+    return res.render("teachers/create")
 })
+
+routes.get('/teachers/:id', teachers.show)
+
+routes.get('/teachers/:id/edit', teachers.edit)
 
 routes.post("/teachers", teachers.post)
+
+routes.put("/teachers", teachers.put)
 
 routes.get('students', function(req, res) {
     return res.send("students")
