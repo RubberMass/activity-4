fb85703bd9f9746862cfd63b22a4ddd98280b3b4
Guilherme Barbosa <64219293+RubberMass@users.noreply.github.com>
2021-03-03 13:11:19 -0300

    Update teachers.js

--- teachers.js
+++ teachers.js
@@ -1,6 +1,30 @@
+const { json } = require('express')
 const fs = require('fs')
 const data = require("./data.json")
-    //post
+const { age, date } = require('./utils')
+
+//show
+exports.show = function(req, res) {
+    const { id } = req.params
+
+    const foundTeachers = data.teachers.find(function(teachers) {
+        return id == teachers.id
+    })
+
+
+    if (!foundTeachers) return res.send("teachers not found!")
+
+    const teachers = {
+        ...foundTeachers,
+        age: age(foundTeachers.birth),
+        services: foundTeachers.services.split(","),
+        created_at: new Intl.DateTimeFormat("pt-BR").format(foundTeachers.created_at),
+    }
+
+    return res.render("teachers/show", { teachers })
+}
+
+//create
 exports.post = function(req, res) {
     const keys = Object.keys(req.body)
     for (key of keys) {
@@ -8,23 +32,74 @@ exports.post = function(req, res) {
             return res.send('please, fill all fields')
     }
 
-    let { avatar_url, name, birth, grau, aula, gender, services } = req.body
+    let { avatar_url, name, birth, grau, aula, services } = req.body
 
     birth = Date.parse(birth)
 
-    data.instructors.push({
+    const id = Number(data.teachers.length + 1)
+
+    data.teachers.push({
+        id,
         avatar_url,
         name,
         birth,
         grau,
         aula,
-        gender,
         services,
+
     })
 
     fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if (err) return res.send("write file error!")
 
-        return res.redirect("/instructors")
+        return res.redirect("/teachers")
     })
+}
+
+//edit
+exports.edit = function(req, res) {
+    const { id } = req.params
+
+    const foundTeachers = data.teachers.find(function(teachers) {
+        return id == teachers.id
+    })
+
+    if (!foundTeachers) return res.send("teachers not found")
+
+    const teachers = {
+        ...foundTeachers,
+        birth: date(foundTeachers.birth)
+    }
+
+    return res.render('teachers/edit', { teachers })
+}
+
+//put
+exports.put = function(req, res) {
+    const { id } = req.body
+    let index = 0
+
+    const foundTeachers = data.teachers.find(function(teachers, foundIndex) {
+        if (id == teachers.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundTeachers) return res.send("teachers not found")
+
+    const teachers = {
+        ...foundTeachers,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.teachers[index] = teachers
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
+        if (err) return res.send("write file error!")
+
+        return res.redirect(`/teachers/${id}`)
+    })
+
 }
